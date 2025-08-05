2300557cc7cad09cbb25d7a297ebf5ac4e6619f9
Nurlan-Aliev <nualiev612@gmail.com>
2025-08-05 13:03:09 +0400

    make relationship

--- api/models.py
+++ api/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import ForeignKey, UniqueConstraint
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from database import Base
 
 
@@ -7,9 +7,18 @@ class Word(Base):
     word: Mapped[str]
     translate_ru: Mapped[str] = mapped_column(nullable=True)
     translate_az: Mapped[str] = mapped_column(nullable=True)
+    word_to_learn = relationship("WordsToLearn", back_populates="word")
 
 
 class LearnedWord(Base):
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     word_id: Mapped[int] = mapped_column(ForeignKey("words.id"))
+    __table_args__ = (UniqueConstraint("user_id", "word_id"),)
+
+
+class WordsToLearn(Base):
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    word_id: Mapped[int] = mapped_column(ForeignKey("words.id"))
+    word = relationship("Word", back_populates="word_to_learn")
+
     __table_args__ = (UniqueConstraint("user_id", "word_id"),)
