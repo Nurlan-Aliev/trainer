c5ca6c4e22a97ee7a59053e76761c9ce65e12390
Nurlan-Aliev <nualiev612@gmail.com>
2025-09-16 22:38:18 +0400

    delete repass

--- auth/crud.py
+++ auth/crud.py
@@ -11,15 +11,8 @@ async def create_user(
     name: str = Form(),
     login: EmailStr = Form(),
     password: str = Form(),
-    re_password: str = Form(),
     session: AsyncSession = Depends(db_helper.session_dependency),
 ):
-    if re_password != password:
-        raise HTTPException(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            detail="Passwords do not match",
-        )
-
     hash_pass = validator.hash_password(password)
     user = await get_user(login, session)
 
