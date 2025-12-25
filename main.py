831e912ac22491b47f856f4cb024935e7a711940
Nurlan-Aliev <nualiev612@gmail.com>
2025-12-25 16:55:33 +0400

    delete cors middleware

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
 from api.routers import router as api_router
 from auth.views import router as auth_router
 from config import settings
@@ -8,12 +7,3 @@ from config import settings
 app = FastAPI(openapi_url="/openapi.json" if settings.DEBUG else None)
 app.include_router(api_router, prefix="/api")
 app.include_router(auth_router, prefix="/auth", tags=["login"])
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
